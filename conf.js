37d797b79fd813e8b69f897962fd26daf3ace716
atishay <atishay.jain@manusis.com>
2015-07-09 12:57:25 +0530

    Minor change -conf.js

--- conf.js
+++ conf.js
@@ -7,6 +7,8 @@
  */
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    chromeOnly: true,
+    chromeDriver: './node_modules/protractor/selenium/chromedriver',
     specs: ['spec.js'],
     capabilities: {
         'browserName': 'chrome'
